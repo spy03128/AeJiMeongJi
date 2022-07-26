431667aee18207afa7b23c103ac52950dc386bdb
TobeyKwon <tobeykwon@gmail.com>
2022-07-26 21:28:12 +0900

    feat : 강아지 프로필 엔티티 추가 #13

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/entity/Dog.java
@@ -0,0 +1,40 @@
+package com.ssafy.aejimeongji.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "dog")
+public class Dog extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthdate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Boolean neutering;
+
+    private Boolean gone;
+
+    private LocalDate adoptedDay;
+
+    public Dog(String name, LocalDate birthdate, Gender gender, Boolean neutering, Boolean gone, LocalDate adoptedDay) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.neutering = neutering;
+        this.gone = gone;
+        this.adoptedDay = adoptedDay;
+    }
+}
