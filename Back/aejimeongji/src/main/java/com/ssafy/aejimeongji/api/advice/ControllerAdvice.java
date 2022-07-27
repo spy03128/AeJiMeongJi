85262f726706ffd0e3993674f049a2b81017bab7
TobeyKwon <tobeykwon@gmail.com>
2022-07-27 19:04:15 +0900

    feat : 예외 핸들러 로그 추가 (#15)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/advice/ControllerAdvice.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/advice/ControllerAdvice.java
@@ -2,15 +2,18 @@ package com.ssafy.aejimeongji.api.advice;
 
 import com.ssafy.aejimeongji.api.dto.ErrorDTO;
 import com.ssafy.aejimeongji.domain.exception.ExpireAuthNumberException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(ExpireAuthNumberException.class)
     public ResponseEntity<ErrorDTO> expireAuthNumberExHandler(Exception ex) {
+        log.error("exception 발생 = {}", ex.getMessage());
         return ResponseEntity.badRequest().body(new ErrorDTO(400, ex.getMessage()));
     }
 }
