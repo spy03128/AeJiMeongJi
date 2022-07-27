30c79fb6e2a1660d59007b78e591f92ee11305fc
TobeyKwon <tobeykwon@gmail.com>
2022-07-27 13:27:55 +0900

    feat : 멤버 서비스 생성 (#14)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.ssafy.aejimeongji.domain.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+public class MemberService {
+}
