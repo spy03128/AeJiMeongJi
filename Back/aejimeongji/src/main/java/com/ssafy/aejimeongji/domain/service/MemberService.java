d0feba48c5b7a603f395dc1ffbd27d7381a97a6f
권도현 <tobeykwon@gmail.com>
2022-07-28 10:12:12 +0900

    feat : 멤버 서비스에 중복 체크 메서드 추가 (#14)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/MemberService.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.ssafy.aejimeongji.domain.service;
 
+import com.ssafy.aejimeongji.domain.condition.DuplicatedCheckCondition;
+import com.ssafy.aejimeongji.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public boolean duplicatedCheck(DuplicatedCheckCondition condition) {
+        return memberRepository.duplicatedCheck(condition);
+    }
 }
