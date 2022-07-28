12edefdd6997bb209d197c86b2a7629c1b2f1958
권도현 <tobeykwon@gmail.com>
2022-07-28 10:22:29 +0900

    feat : MemberService에 findMember 메서드 추가 (#14)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/MemberService.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.ssafy.aejimeongji.domain.service;
 
 import com.ssafy.aejimeongji.domain.condition.DuplicatedCheckCondition;
+import com.ssafy.aejimeongji.domain.entity.Member;
+import com.ssafy.aejimeongji.domain.exception.MemberNotFoundException;
 import com.ssafy.aejimeongji.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,5 +20,10 @@ public class MemberService {
 
     public boolean duplicatedCheck(DuplicatedCheckCondition condition) {
         return memberRepository.duplicatedCheck(condition);
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException());
     }
 }
