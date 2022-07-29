e90c6ca692072a0f749969b18c68c8a674f08a4e
박경열 <hrq789@naver.com>
2022-07-29 14:57:59 +0900

    feat : DTO 추가 (#18)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/CoordinateRequest.java
@@ -0,0 +1,11 @@
+package com.ssafy.aejimeongji.api.dto;
+
+import lombok.Data;
+
+@Data
+public class CoordinateRequest {
+
+    private String lat;
+    private String len;
+
+}
---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/PetPlaceResponse.java
@@ -0,0 +1,28 @@
+package com.ssafy.aejimeongji.api.dto;
+
+import com.ssafy.aejimeongji.domain.entity.PetPlace;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PetPlaceResponse {
+
+    private String name;
+    private String description;
+    private String address;
+    private String tel;
+    private String category;
+    private String openingHours;
+
+    public PetPlaceResponse(PetPlace petPlace) {
+
+        name = petPlace.getName();
+        description = petPlace.getDescription();
+        address = petPlace.getAddress();
+        tel = petPlace.getTel();
+        category = petPlace.getCategory();
+        openingHours = petPlace.getOpeningHours();
+
+    }
+}
