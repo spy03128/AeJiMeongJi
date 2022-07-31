1c0e5ea68dbba943b50fd6b8146e542e7d0cbd54
박경열 <hrq789@naver.com>
2022-08-01 00:37:37 +0900

    feat : 캘린더 생성 DTO 개발 (#32)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/Calendar/UpdateCalendarRequest.java
@@ -0,0 +1,19 @@
+package com.ssafy.aejimeongji.api.dto.Calendar;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCalendarRequest {
+
+    private String title;
+    private String content;
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private LocalDate date;
+}
