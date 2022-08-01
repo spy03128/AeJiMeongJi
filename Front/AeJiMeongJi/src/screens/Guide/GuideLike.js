d9d5bbb3d4bd361e6762ac660c6e2823a4713e41
kimchaeri <kcr7812@naver.com>
2022-08-01 17:51:20 +0900

    feat : 가이드 디테일 공유하기(#34)

--- Front/AeJiMeongJi/src/screens/Guide/GuideLike.js
+++ Front/AeJiMeongJi/src/screens/Guide/GuideLike.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {Image, StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, Share, View, TouchableOpacity, Image} from 'react-native';
 
 const GuideLike = () => {
-  return;
+  return (
+    <View style={{marginTop: 0, width: '10%'}}>
+      <TouchableOpacity>
+        <Image
+          style={styles.shareLogo}
+          resizeMode="contain"
+          source={require('../../Assets/image/empty-heart.png')}
+          title="Calendar"
+        />
+      </TouchableOpacity>
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  shareLogo: {
+    width: '100%',
+    height: 55,
+  },
+});
 
 export default GuideLike;
