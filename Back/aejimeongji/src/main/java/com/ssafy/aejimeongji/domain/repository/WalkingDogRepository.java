c648d459a358ce3a967767bc1798ed817ce04bcb
TobeyKwon <tobeykwon@gmail.com>
2022-08-02 13:23:43 +0900

    feat : WalkingDogRepository findDogId 추가 (#33)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/repository/WalkingDogRepository.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/repository/WalkingDogRepository.java
@@ -1,14 +1,14 @@
 package com.ssafy.aejimeongji.domain.repository;
 
 import com.ssafy.aejimeongji.domain.entity.WalkingDog;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface WalkingDogRepository extends JpaRepository<WalkingDog, Long> {
 
-    @Query("select wd from WalkingDog wd join fetch wd.dog d join fetch wd.walking where wd.dog = :dogId")
+    @Query("select wd from WalkingDog wd join fetch wd.dog d join fetch wd.walking w where wd.dog = :dogId")
     List<WalkingDog> findByDogId(@Param("dogId") Long dogId);
 }
