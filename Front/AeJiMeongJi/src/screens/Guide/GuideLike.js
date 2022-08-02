e330bc154dfd5d6b5e75339d6a1ef9963ac96dfe
kimchaeri <kcr7812@naver.com>
2022-08-02 14:45:33 +0900

    feat : 가이드 좋아요 버튼 애니메이션(#34)

--- Front/AeJiMeongJi/src/screens/Guide/GuideLike.js
+++ Front/AeJiMeongJi/src/screens/Guide/GuideLike.js
@@ -1,25 +1,70 @@
 import React from 'react';
-import {StyleSheet, Share, View, TouchableOpacity, Image} from 'react-native';
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  Extrapolate,
+  interpolate,
+} from 'react-native-reanimated';
+import {Pressable, View, Button, StyleSheet, Image} from 'react-native';
 
-const GuideLike = () => {
+const LikeButton = () => {
+  const liked = useSharedValue(0);
+
+  const outlineStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(liked.value, [0, 1], [1, 0], Extrapolate.CLAMP),
+        },
+      ],
+    };
+  });
+
+  const fillStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: liked.value,
+        },
+      ],
+      opacity: liked.value,
+    };
+  });
+
   return (
-    <View style={{marginTop: 0, width: '10%'}}>
-      <TouchableOpacity>
+    <Pressable onPress={() => (liked.value = withSpring(liked.value ? 0 : 1))}>
+      <Animated.View style={[StyleSheet.absoluteFillObject, outlineStyle]}>
         <Image
           style={styles.shareLogo}
           resizeMode="contain"
           source={require('../../Assets/image/empty-heart.png')}
           title="Calendar"
         />
-      </TouchableOpacity>
-    </View>
+      </Animated.View>
+      <Animated.View style={fillStyle}>
+        <Image
+          style={styles.shareLogo}
+          resizeMode="contain"
+          source={require('../../Assets/image/fill-heart.png')}
+          title="Calendar"
+        />
+      </Animated.View>
+    </Pressable>
   );
 };
+
 const styles = StyleSheet.create({
   shareLogo: {
-    width: '100%',
+    width: 40,
     height: 55,
   },
 });
 
-export default GuideLike;
+export default function GuideLike(props) {
+  return (
+    <View style={{}}>
+      <LikeButton />
+    </View>
+  );
+}
