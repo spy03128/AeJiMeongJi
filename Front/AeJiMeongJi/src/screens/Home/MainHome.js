d7a110fb6d88a97e5ad8d425626c7f3d7fa81f8f
jihyeon <spy03128@naver.com>
2022-08-03 02:48:54 +0900

    feat : 산책 화면 컴포넌트 분리 및 산책화면 구성,디자인 변경 (#7)

--- Front/AeJiMeongJi/src/screens/Home/MainHome.js
+++ Front/AeJiMeongJi/src/screens/Home/MainHome.js
@@ -9,11 +9,9 @@ import {
 } from 'react-native';
 import {Colors} from '../../constants/styles';
 import Profile from '../../components/Home/Profile';
+import Running from '../../components/Home/Running';
 
 const MainHome = ({navigation}) => {
-  const num = 0;
-  const min = 20;
-  const km = 1.4;
   var [isPress, setIsPress] = React.useState(false);
   return (
     <ScrollView>
@@ -62,36 +60,7 @@ const MainHome = ({navigation}) => {
             </View>
           </View>
 
-          <View style={styles.runningbox}>
-            <View style={styles.run1}>
-              <Image
-                style={styles.runicon}
-                resizeMode="contain"
-                source={require('../../Assets/image/run-icon.png')}
-              />
-            </View>
-
-            <View style={styles.run2}>
-              <Text style={[styles.font, styles.mainText]}>
-                이번주 산책횟수
-                <Text style={{color: Colors.btnBack100}}> {num}</Text>
-                <Text>회</Text>
-              </Text>
-              {num == 0 ? (
-                <Text style={[styles.font, styles.subText]}>
-                  산책이 필요해요😂
-                </Text>
-              ) : (
-                <Text style={[styles.font, styles.subText]}>
-                  최고기록
-                  <Text style={{color: Colors.btnBack100}}> {min}</Text>
-                  <Text>분</Text>
-                  <Text style={{color: Colors.btnBack100}}> {km}</Text>
-                  <Text>km</Text>
-                </Text>
-              )}
-            </View>
-          </View>
+          <Running />
 
           <View style={styles.box}>
             <View
@@ -372,74 +341,6 @@ const styles = StyleSheet.create({
     marginTop: 50,
     maxWidth: '60%',
     maxHeight: '30%',
-  },
-  profile: {
-    flex: 1.3,
-    height: 150,
-    marginTop: 40,
-    marginBottom: 20,
-    alignSelf: 'center',
-    width: '80%',
-    flexDirection: 'row',
-  },
-  profile1: {
-    flex: 1,
-    width: '50%',
-    padding: 10,
-  },
-  profile1sub1: {
-    flex: 2.5,
-    borderRadius: 20,
-    marginBottom: 10,
-    backgroundColor: Colors.contentBox,
-    borderColor: Colors.btnBack100,
-    borderWidth: 2,
-    flexDirection: 'row',
-  },
-  name1: {
-    flex: 1,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    paddingLeft: 5,
-  },
-  name2: {
-    flex: 2.5,
-    flexDirection: 'row-reverse',
-    justifyContent: 'center',
-  },
-  profile1sub2: {
-    flex: 5,
-  },
-  profile2: {
-    flex: 1,
-  },
-  runningbox: {
-    height: 350,
-    alignSelf: 'center',
-    backgroundColor: Colors.contentBox,
-    width: '80%',
-    marginTop: 20,
-    marginBottom: 20,
-    borderRadius: 20,
-    flexDirection: 'row',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-
-    elevation: 8,
-  },
-  run1: {
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  run2: {
-    flex: 3.5,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 
   guidebox: {
