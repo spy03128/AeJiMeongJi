859009d340daffd422ff468affd7775a70e1e390
TobeyKwon <tobeykwon@gmail.com>
2022-08-10 21:03:55 +0900

    refact : 가이드북 request 수정 (#64)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/guidebook/GuideBookRequest.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/guidebook/GuideBookRequest.java
@@ -17,14 +17,22 @@ public class GuideBookRequest {
     private String title;
     private String content;
     private String category;
-    private int monthMin;
-    private int monthMax;
-    private int weightMin;
-    private int weightMax;
+    private Integer monthMin;
+    private Integer monthMax;
+    private Integer weightMin;
+    private Integer weightMax;
     private MultipartFile thumbnail;
 
     public GuideBook convertGuideBook() throws IOException {
-        return GuideBook.builder().title(title).content(content).category(category).monthMin(monthMin).monthMax(monthMax).weightMin(weightMin).weightMax(weightMax).build();
+        return GuideBook.builder()
+                .title(title)
+                .content(content)
+                .category(category)
+                .monthMin(monthMin != null ? monthMin : 9999)
+                .monthMax(monthMax != null ? monthMax : 9999)
+                .weightMin(weightMin != null ? weightMin : 9999)
+                .weightMax(weightMax != null ? weightMax : 9999)
+                .build();
     }
 
     public GuideBook toUpdateParam() throws IOException {
