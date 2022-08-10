7be6d9409c93b063887b6214ba48e4657afe2ad3
TobeyKwon <tobeykwon@gmail.com>
2022-08-10 20:51:56 +0900

    refact : 가이드 DTO 변경 (#64)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/guidebook/GuideBookRequest.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/guidebook/GuideBookRequest.java
@@ -8,6 +8,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,11 +17,17 @@ public class GuideBookRequest {
     private String title;
     private String content;
     private String category;
-    private int dogAge;
-    private int dogWeight;
+    private int monthMin;
+    private int monthMax;
+    private int weightMin;
+    private int weightMax;
     private MultipartFile thumbnail;
 
-    public GuideBook convertGuideBook(Image image) {
-        return new GuideBook(getTitle(), getContent(), getCategory(), getDogAge(), getDogWeight(), new GuideThumbnail(image));
+    public GuideBook convertGuideBook() throws IOException {
+        return GuideBook.builder().title(title).content(content).category(category).monthMin(monthMin).monthMax(monthMax).weightMin(weightMin).weightMax(weightMax).build();
+    }
+
+    public GuideBook toUpdateParam() throws IOException {
+        return GuideBook.builder().title(title).content(content).category(category).monthMin(monthMin).monthMax(monthMax).weightMin(weightMin).weightMax(weightMax).build();
     }
 }
