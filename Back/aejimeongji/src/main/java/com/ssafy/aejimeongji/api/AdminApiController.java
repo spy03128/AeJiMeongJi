e8fa724ec5d3bd0cc17ebb79af62338e8f8b9652
TobeyKwon <tobeykwon@gmail.com>
2022-08-10 21:06:59 +0900

    refact : 어드민 컨트롤러 출력 메시지 수정 (#64)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/AdminApiController.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/AdminApiController.java
@@ -22,20 +22,20 @@ public class AdminApiController {
     public ResponseEntity<ResponseDTO> saveGuideBook(@ModelAttribute GuideBookRequest request) throws IOException {
         log.info("가이드북 등록 요청");
         guideBookService.saveGuideBook(request.convertGuideBook(), request.getThumbnail());
-        return ResponseEntity.ok(new ResponseDTO("가이드북 등록이 완료되었습니다."));
+        return ResponseEntity.ok(new ResponseDTO("가이드 등록이 완료되었습니다."));
     }
 
     @PutMapping("/guide/{guideId}")
     public ResponseEntity<ResponseDTO> updateGuideBook(@PathVariable Long guideId, @ModelAttribute GuideBookRequest request) throws IOException {
         log.info("가이드북 {} 수정 요청", guideId);
         Long updatedId = guideBookService.updateGuideBook(guideId, request.convertGuideBook(), request.getThumbnail());
-        return ResponseEntity.ok(new ResponseDTO("가이드북 " + updatedId + " 수정이 완료되었습니다."));
+        return ResponseEntity.ok(new ResponseDTO(guideId + "번 가이드 수정이 완료되었습니다."));
     }
 
     @DeleteMapping("/guide/{guideId}")
     public ResponseEntity<ResponseDTO> deleteGuideBook(@PathVariable Long guideId) {
         log.info("가이드북 {} 삭제 요청", guideId);
         guideBookService.deleteGuideBook(guideId);
-        return ResponseEntity.ok().body(new ResponseDTO("가이드북 " + guideId + " 삭제가 완료되었습니다."));
+        return ResponseEntity.ok().body(new ResponseDTO(guideId + "번 가이드북 삭제가 완료되었습니다."));
     }
 }
